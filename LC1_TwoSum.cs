b527651f8895acda881942c1747d893ec02fc6ca
madbyit <madbyit@bystedts.com>
2022-03-26 15:30:54 +0100

    Add commenting to LC1

--- LC1_TwoSum.cs
+++ LC1_TwoSum.cs
@@ -2,6 +2,16 @@ using System;
 
 namespace LeetCode
 {
+    /// <summary>
+    /// Leetcode 1: Two Sum
+    /// </summary>
+
+        /// <summary>
+        /// Given an array of integers nums and an integer target, 
+        /// return indices of the two numbers such that they add up to target
+        /// </summary>
+        /// <param name="nums, target"></param>
+        /// <returns>int array</returns>
 
     public class Solution
     {
@@ -35,6 +45,9 @@ namespace LeetCode
             return solution;
         }
 
+        /// <summary>
+        /// Main to run program
+        /// </summary>
         static void Main(string[] args)
         {
             Solution s = new Solution();
@@ -54,29 +67,24 @@ namespace LeetCode
 
 //PROBLEM
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 // You can return the answer in any order.
- 
 
 // Example 1:
-
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
 
+// Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
-// Example 3:
 
+// Example 3:
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
  
 
 // Constraints:
-
 // 2 <= nums.length <= 104
 // -109 <= nums[i] <= 109
 // -109 <= target <= 109
