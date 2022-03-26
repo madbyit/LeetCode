c0308bb209fd606d1e78757c54a3d62dc63a36d1
madbyit <madbyit@bystedts.com>
2022-03-26 15:25:48 +0100

    Add accetable answer to Challenge 1342

---
+++ LC1342_NumberOfStepsToReduceANumberToZero.cs
@@ -0,0 +1,90 @@
+using System;
+
+namespace LeetCode
+{
+
+/// <summary>
+    /// Leetcode 1342: Number of Steps to Reduce a Number to Zero
+    /// </summary>
+
+        /// <summary>
+        /// Number of steps to zero
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns>int</returns>
+
+    public class Solution
+    {
+
+        public int NumberOfSteps(int num) {
+
+            int steps = 0;
+
+            do
+            {
+                if(num == 0)
+                {
+                    break;
+                }
+                // Check if even
+                else if(num % 2 == 0)
+                {
+                    num = num/2;
+                    steps++;
+                }
+                else
+                {
+                    num = num-1;
+                    steps++;
+                }
+
+            }while(num != 0);
+            
+            return steps;
+        }
+
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int steps = s.NumberOfSteps(0);
+
+            Console.WriteLine("Number of steps: " + steps);
+        }
+    }
+
+}
+
+
+
+//PROBLEM
+/*
+Given an integer num, return the number of steps to reduce it to zero.
+In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+Example 1:
+Input: num = 14
+Output: 6
+Explanation: 
+Step 1) 14 is even; divide by 2 and obtain 7. 
+Step 2) 7 is odd; subtract 1 and obtain 6.
+Step 3) 6 is even; divide by 2 and obtain 3. 
+Step 4) 3 is odd; subtract 1 and obtain 2. 
+Step 5) 2 is even; divide by 2 and obtain 1. 
+Step 6) 1 is odd; subtract 1 and obtain 0.
+
+Example 2
+Input: num = 8
+Output: 4
+Explanation: 
+Step 1) 8 is even; divide by 2 and obtain 4. 
+Step 2) 4 is even; divide by 2 and obtain 2. 
+Step 3) 2 is even; divide by 2 and obtain 1. 
+Step 4) 1 is odd; subtract 1 and obtain 0.
+
+Example 3:
+Input: num = 123
+Output: 12
+
+Constraints:
+0 <= num <= 106
+*/
